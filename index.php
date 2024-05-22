8c48e4415247753a4eb20fef9336c98129d8e95c
Malicsuave <reywillardd01@gmail.com>
2024-05-22 10:13:45 +0800

    icon and profile

--- index.php
+++ index.php
@@ -35,7 +35,9 @@ if (isset( $_POST['Delete'])) {
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
   <!-- For Icons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-<link rel="stylesheet" href="./includes/style.css">
+  <!-- foricons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 </head>
 <body>
   <?php include ('includes/navbar.php'); ?>
@@ -81,16 +83,21 @@ if (isset( $_POST['Delete'])) {
           <td><?php echo $rows ['address']; ?></td>
           
           <td>
-        <form action="update.php"method="POST" style="display: inline;">
-            <input type="hidden" name="id" value="<?php echo $rows ['User_Id']; ?>">
-            <input type="submit" class="btn btn-primary btn-sm" name="Update" value="Update" onclick="return confirm('Are you sure you want to update this user?')">
-        </form>
-        <!-- Delete button -->
-        <form method="POST" style="display: inline;">
-            <input type="hidden" name="id" value="<?php echo $rows ['User_Id']; ?>">
-            <input type="submit" class="btn btn-danger btn-sm" name="Delete" value="Delete" onclick="return confirm('Are you sure you want to delete this user?')">
-        </form>
-          </td>
+          <div class="btn-group" role="group">
+          <form action="update.php" method="post" class="d-inline">
+                                    <input type="hidden" name="id" value="<?php echo $rows['User_Id']; ?>">
+                                    <button type="submit" class="btn btn-warning ">
+                                    <i class="fa-solid fa-pen-to-square"></i>
+                                    </button>
+                                </form>
+                                <form method="POST" class="d-inline">
+                                    <input type="hidden" name="id" value="<?php echo $rows['User_Id']; ?>">
+                                    <button type="submit" name="delete" class="btn btn-danger " onclick="return confirm('Are you sure you want to delete this user?')">
+                                    <i class="fa-solid fa-user-minus"></i>
+                                    </button>
+                                </form>
+        </div>
+        </td>
         </tr>
     <?php
     }
@@ -99,9 +106,90 @@ if (isset( $_POST['Delete'])) {
       </tbody>
     </table>
   </div>
-</div>
-</div>
+  <div class="container my-5">
+        <h2 class="text-center">User Profiles</h2>
+        <div class="card-container">
+            <?php
+            $data = $con->view();
+            foreach ($data as $rows) {
+            ?>
+            <div class="card">
+                <div class="card-body text-center">
+                    <?php if (!empty($rows['user_profile_picture'])): ?>
+                        <img src="<?php echo htmlspecialchars($rows['user_profile_picture']); ?>" alt="Profile Picture" class="profile-img">
+                    <?php else: ?>
+                        <img src="path/to/default/profile/pic.jpg" alt="Default Profile Picture" class="profile-img">
+                    <?php endif; ?>
+                    <h5 class="card-title"><?php echo htmlspecialchars($rows['firstname']) . ' ' . htmlspecialchars($rows['lastname']); ?></h5>
+                    <p class="card-text"><strong>Birthday:</strong> <?php echo htmlspecialchars($rows['birthday']); ?></p>
+                    <p class="card-text"><strong>Sex:</strong> <?php echo htmlspecialchars($rows['sex']); ?></p>
+                    <p class="card-text"><strong>Username:</strong> <?php echo htmlspecialchars($rows['username']); ?></p>
+                    <p class="card-text"><strong>Address:</strong> <?php echo ucwords(htmlspecialchars($rows['address'])); ?></p>
+                    <form action="update.php" method="post" class="d-inline">
+                        <input type="hidden" name="id" value="<?php echo htmlspecialchars($rows['User_Id']); ?>">
+                        <button type="submit" class="btn btn-primary btn-sm">Edit</button>
+                    </form>
+                    <form method="POST" class="d-inline">
+                        <input type="hidden" name="id" value="<?php echo htmlspecialchars($rows['User_Id']); ?>">
+                        <input type="submit" name="delete" class="btn btn-danger btn-sm" value="Delete" onclick="return confirm('Are you sure you want to delete this user?')">
+                    </form>
+                </div>
+            </div>
+            <?php
+            }
+            ?>
+        </div>
+    </div>
 
+ <!-- HTML declaration for bar graph -->
+  <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+
+  <!-- HTML declaration for pie chart -->
+  <div id="pie" style="height: 370px; width: 100%;"></div>
+
+</div>
+<!-- Combine both chart scripts inside one window.onload -->
+<script>
+window.onload = function() {
+  var userChart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    theme: "light2",
+    title: {
+      text: "Numbers of Users based on Sex"
+    },
+    axisY: {
+      title: "Number of Users per Sex"
+    },
+    data: [{
+      type: "column",
+      yValueFormatString: "",
+      dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+    }]
+  });
+  userChart.render();
+
+  var expenseChart = new CanvasJS.Chart("pie", {
+    animationEnabled: true,
+    exportEnabled: true,
+    title: {
+      text: "Number of Users per Sex"
+    },
+    subtitles: [{
+      text: "Total"
+    }],
+    data: [{
+      type: "pie",
+      showInLegend: "true",
+      legendText: "{label}",
+      indexLabelFontSize: 16,
+      indexLabel: "{label} - #percent%",
+      yValueFormatString: "#,##0",
+      dataPoints: <?php echo json_encode($datapoints, JSON_NUMERIC_CHECK); ?>
+    }]
+  });
+  expenseChart.render();
+}
+</script>
 <!-- Bootstrap JS and dependencies -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
@@ -109,6 +197,8 @@ if (isset( $_POST['Delete'])) {
 <script src="./bootstrap-5.3.3-dist/js/bootstrap.js"></script>
 <!-- Bootsrap JS na nagpapagana ng danger alert natin -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<!-- For Charts -->
+<script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
 
 </body>
 </html>
